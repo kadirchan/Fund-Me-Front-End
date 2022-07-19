9d064c85a17ecadcbe441892497964bc84a8eb21
Kadircan Bozkurt <67785258+kadirchan@users.noreply.github.com>
2022-07-19 03:27:35 +0300

    Add files via upload

---
+++ html-fund-me/index.js
@@ -0,0 +1,81 @@
+//front-end js de require yok import var
+import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const balanceButton = document.getElementById("balanceButton");
+const withdrawButton = document.getElementById("withdrawButton");
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
+withdrawButton.onclick = withdraw;
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" });
+        } catch (error) {
+            console.log(error);
+        }
+        document.getElementById("connectButton").innerHTML = "Connected!";
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+        console.log(accounts);
+    } else {
+        document.getElementById("connectButton").innerHTML =
+            "Install Metamsk dumbass!";
+    }
+}
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const balance = await provider.getBalance(contractAddress);
+        console.log(ethers.utils.formatEther(balance));
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value;
+    console.log(`Funding ${ethAmount} ETH ...`);
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+                gasLimit: 3000000,
+            });
+            await listenForTransactionMine(transactionResponse, provider);
+            console.log("Done!");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash} ...`);
+
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (trasactionReceipt) => {
+            console.log(
+                `Completed with ${trasactionReceipt.confirmations} block confirmations`
+            );
+            resolve();
+        });
+    });
+}
+
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        try {
+            const transactionResponse = await contract.withdraw();
+            await listenForTransactionMine(transactionResponse, provider);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
